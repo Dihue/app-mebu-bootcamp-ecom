c42b487c468b06d62bff855ceefa63962141e298
Dihue <dihue21@gmail.com>
2024-11-21 01:10:35 -0300

    Detalles y lista de Usuarios

--- src/apps/usuarios/urls.py
+++ src/apps/usuarios/urls.py
@@ -6,5 +6,7 @@ app_name = 'usuarios'
 
 
 urlpatterns = [
-    path('nuevo/', views.Nuevo.as_view(), name='nuevo')
+    path('nuevo/', views.Nuevo.as_view(), name='nuevo'),
+    path('lista/', views.Lista.as_view(), name='lista'),
+    path('usuario/<int:id>/', views.UsuarioDetalles.as_view(), name='detalle'),
 ]
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
 from .forms import FormUsuario
@@ -15,4 +17,29 @@ class Nuevo(CreateView):
         ctx = super(Nuevo, self).get_context_data(**kwargs)
         ctx["titulo"] = "Nuevo registro"
         ctx["subtitulo"] = "Registro"
+        return ctx
+
+
+class UsuarioDetalles(DetailView):
+    model = Usuario
+    template_name = 'usuarios/detalle.html'
+    context_object_name = 'usuario'
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(UsuarioDetalles, self).get_context_data(**kwargs)
+        ctx["titulo"] = "Detalles del Usuario"
+        ctx["subtitulo"] = "Detalles"
+        return ctx
+
+
+class Lista(ListView):
+    template_name = 'usuarios/lista.html'
+    model = Usuario
+    context_object_name = 'usuario'
+    paginate_by = 3
+    
+    def get_context_data(self, **kwargs):
+        ctx = super(Lista, self).get_context_data(**kwargs)
+        ctx["titulo"] = "Lista de Usuarios"
         return ctx
